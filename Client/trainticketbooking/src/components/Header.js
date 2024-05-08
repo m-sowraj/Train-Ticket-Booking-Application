c978b7b6f7cd24ed7f9ba279b16dde5d87b484e6
m-sowraj <140877454+m-sowraj@users.noreply.github.com>
2024-05-08 22:28:32 +0530

    added logout option

--- Client/trainticketbooking/src/components/Header.js
+++ Client/trainticketbooking/src/components/Header.js
@@ -3,31 +3,45 @@ import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const navigate = useNavigate();
+  const authToken = localStorage.getItem('authToken');
+
+  
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    navigate('/');
+    window.location.reload();
+  };
+  
 
   return (
     <div style={styles.container}>
       <h2 style={styles.heading}>TrainTickets</h2>
-           
-      <button onClick={() => {navigate('/orderHistory')}} style={styles.getStartedButton}>Order History</button>
+      <div>
+        {authToken ? (
+          <>
+            <button onClick={() => navigate('/orderHistory')} style={styles.getStartedButton}>Order History</button>
+            <button onClick={handleLogout} style={styles.getStartedButton}>Logout</button>
+          </>
+        ) : (
+          <button style={styles.getStartedButton}>Login</button>
+        )}
+      </div>
     </div>
   );
 };
-
-export default Header;
 
 const styles = {
   container: {
     backgroundColor: '#121826',
     color: '#ffffff',
     padding: 30,
-    display: 'flex', 
-    flexDirection: 'row', 
+    display: 'flex',
+    flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center', 
-    position:'fixed',
-    width:'96%',
-    height:'3%',
-    
+    alignItems: 'center',
+    position: 'fixed',
+    width: '96%',
+    height: '3%',
   },
   heading: {
     fontSize: '35px',
@@ -37,7 +51,10 @@ const styles = {
     color: '#ffffff',
     border: 'none',
     padding: '10px 20px',
+    margin: '10px',
     borderRadius: '5px',
     cursor: 'pointer',
   },
 };
+
+export default Header;
